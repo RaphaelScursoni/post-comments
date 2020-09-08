30e2b84273cb8f09fb74d650cf3a4bf142a7a92c
Raphael Scursoni <ra.scursoni@gmail.com>
2020-09-08 11:16:39 -0300

    Atualização final

--- SocialMedia/Entities/Comment.cs
+++ SocialMedia/Entities/Comment.cs
@@ -6,5 +6,13 @@ namespace SocialMedia.Entities
 {
     class Comment
     {
+        public string Text { get; set; }
+
+        public Comment() { }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
     }
 }
--- SocialMedia/Program.cs
+++ SocialMedia/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using SocialMedia.Entities;
 
 namespace SocialMedia
 {
@@ -6,7 +7,28 @@ namespace SocialMedia
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Comment c1 = new Comment("Wow, that's awesome!");
+            Comment c2 = new Comment("Have a nice Trip!");
+            Post p1 = new Post(
+                DateTime.Parse("21/06/2018 13:05:44"),
+                "Treveling to New Zelland",
+                "I'm going to visit this bealtiful country!",
+                12);
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            Comment c3 = new Comment("Good Night!");
+            Comment c4 = new Comment("May the force be with you");
+            Post p2 = new Post(
+                DateTime.Parse("28/07/2020 23:14:19"),
+                "Good night guys",
+                "See you tomorrow!",
+                5);
+            p1.AddComment(c3);
+            p1.AddComment(c4);
+
+                );
+
         }
     }
 }
